6b940a1cfd1e6d5ff7b578c1bd5d62728935af1f
pavel rybakov <pavel.r.2015@mail.ru>
2025-05-22 16:51:33 +0500

    Исправил.

--- users/tests.py
+++ users/tests.py
@@ -4,34 +4,31 @@ from rest_framework.test import APITestCase
 from users.models import User
 
 
-class RegistrationApiViewTest(APITestCase):
-    """
-    Тесты для API регистрации пользователя.
-    """
+class UserAPITestCase(APITestCase):
 
     def setUp(self):
-        """
-        Настройка перед каждым тестом.
-        """
-        pass
+        self.user = User.objects.create(email="test@example.com")
+        self.client.force_authenticate(user=self.user)
 
-    def test_registration_api_view(self):
-        """
-        Проверяет успешную регистрацию нового пользователя через API.
-        Отправляет POST-запрос на эндпоинт регистрации и проверяет, что пользователь создан.
-        """
-        registration_data = {
-            "email": "testuser@example.com",
-            "password": "testpassword",
-        }
-        response = self.client.post('/users/register/', registration_data)
-
+    def test_user_register(self):
+        """Тестирование регистрации пользователя"""
+        data = {"email": "test@test.com", "password": "123test"}
+        response = self.client.post("/users/register/", data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        user = User.objects.get(email="testuser@example.com")
-        self.assertIsNotNone(user)
 
-    def tearDown(self):
-        """
-        Очистка после выполнения теста.
-        """
-        pass
+    def test_user_update(self):
+        """Тестирование обновления профиля пользователя"""
+
+        data = {"first_name": "User", "last_name": "Test"}
+        response = self.client.patch(f"/users/{self.user.pk}/update/", data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_detail_user(self):
+        """Тестирование просмотра профиля пользователя"""
+        response = self.client.get(f"/users/{self.user.pk}/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_user(self):
+        """Тестирование удаления пользователя"""
+        response = self.client.delete(f"/users/{self.user.pk}/delete/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
