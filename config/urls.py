95e7e8b8a52d0317ddb3e4acb354d4fae9a2b541
pavel rybakov <pavel.r.2015@mail.ru>
2025-05-25 15:16:16 +0500

    Всё готово. контейнеры работают.

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
